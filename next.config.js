cb22128a159eeba3fa0acc5e1015d06180f238d3
Sergey <redmoloch@yanadex.ru>
2022-04-30 21:20:46 +0300

    added redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/old-products',
+        destination: '/products',
+        permanent: false, // 307
+      },
+      {
+        source: '/old-products/:id',
+        destination: '/products/:id',
+        permanent: true, // 308
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig

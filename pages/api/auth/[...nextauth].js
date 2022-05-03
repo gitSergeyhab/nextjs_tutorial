da7bc67e6e6cfe85538683c38ad06f00bbe054d2
Sergey <redmoloch@yandex.ru>
2022-05-03 16:53:12 +0300

    added mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,15 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "lib/mongodb";
 
 export default NextAuth({
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
-        })
-    ]
+        }),
+    ],
+    adapter: MongoDBAdapter(clientPromise),
+
 })
